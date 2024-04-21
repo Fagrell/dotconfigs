6a0849ef85f4abf9d0a58aca979a36f6c8b48ec7
Alexander Fagrell <fagrell@outlook.com>
2024-04-21 10:09:11 +0100

    Support tf lsp

--- neovim/lsp.lua
+++ neovim/lsp.lua
@@ -68,3 +68,10 @@ require('lspconfig')['ltex'].setup {
 
 require('toggle_lsp_diagnostics').init()
 
+require('lspconfig').terraformls.setup {}
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.tf", "*.tfvars"},
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
